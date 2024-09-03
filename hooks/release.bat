7dd44d0c3f51874d3c2165038a3397bc96df9c82
胡琤波 <657225485@qq.com>
2024-09-03 13:10:20 +0800

    0.1.14 release

--- hooks/release.bat
+++ hooks/release.bat
@@ -79,13 +79,17 @@ powershell -Command "(Get-Content '!project_file!') -replace '<PackageVersion>!c
 echo Version updated for !project_name! (!project_file!)
 
 REM Stage the changed project file
-git add .
+git add "!project_file!"
+
+REM Commit the version change
+git commit -m "Bump version to !new_version!"
 
 REM Create an annotated tag with the changes
-git tag -a v%new_version% -m "Bump version to %new_version%"
+git tag -a v!new_version! -m "Bump version to !new_version!"
 
-REM Push the tag to main (this includes the changes)
-git push origin v%new_version%
+REM Push the changes and tag to main
+git push origin main
+git push origin v!new_version!
 
 REM Switch to develop branch and merge changes from main
 git checkout develop
