5a416215b69db6c8d4c1bd6aa1813434af9a44d4
胡琤波 <657225485@qq.com>
2024-09-03 11:33:22 +0800

    pre-push

--- hooks/pre-push.bat
+++ hooks/pre-push.bat
@@ -9,8 +9,26 @@ for /f "delims=" %%i in ('git rev-parse --abbrev-ref HEAD') do set "current_bran
 if "!current_branch!" == "develop" (
     echo Current branch is develop. Running pre-push checks...
 
-    REM Add your original develop branch pre-push logic here
-    REM For example, running tests or other checks
+    echo Merging develop branch to main...
+
+    REM Switch to main branch and update
+    git checkout main
+    git pull origin main
+
+    REM Get the last commit message from develop branch
+    for /f "delims=" %%i in ('git log develop -1 --pretty^=%%s') do set "last_commit_msg=%%i"
+
+    REM Merge develop branch, using --squash option to compress all commits into one
+    git merge --squash develop
+
+    REM Commit with the last commit message
+    git commit -m "!last_commit_msg!"
+
+    REM Uncomment the following line if you want to automatically push to main
+    REM git push origin main
+
+    echo Merge completed. Please review the changes and push manually if everything looks good.
+
 
     echo Pre-push checks for develop branch completed.
 ) else if "!current_branch!" == "main" (
